269661764d929187121c2870aad1ff551b2f4a23
ruansilva123 <ruancplay59@gmail.com>
2024-09-11 10:21:39 -0300

    Feature: ajuste da URI para recebimento das fotos

--- MedicionalAnalisys/main.py
+++ MedicionalAnalisys/main.py
@@ -1,19 +1,32 @@
-# fastapi libs
+# FastAPI libs
+from fastapi import FastAPI, File, UploadFile, status, HTTPException
+from fastapi.responses import JSONResponse
 
-from fastapi import FastAPI, HTTPException
-from typing import List, Optional
-from sqlalchemy.exc import IntegrityError
-import sqlite3
-from typing import Any, Dict
 
-# myapp
+# Project libs
 
-app = FastAPI()
 
-# endpoints
 
-@app.post("/use_model/")
-async def root():
-    query = clubs.select()
-    return await database.fetch_all(query)
+# Config project
+app = FastAPI(debug=True)
 
+
+@app.post("/upload-image/")
+async def process_image(file: UploadFile):
+    
+    # Verifica tipo de arquivo
+    if file.content_type not in ["image/jpeg", "image/png"]:
+        return JSONResponse(content={"error" : "Invalid file type!"}, status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+    # Salva arquivo no servidor
+    with open(f"uploaded_images/{file.filename}", "wb") as image:
+        content = await file.read()
+        image.write(content)
+    
+    return JSONResponse(content={"message" : "OK"}, status_code=status.HTTP_200_OK)
+
+
+# Inicializar servidor
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True)
